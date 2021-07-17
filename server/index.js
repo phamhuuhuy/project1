c6833c7843537bd3762d945205c7df0906c12c64
Huu Huy <s3817719@rmit.edu.vn>
2021-07-17 21:43:01 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 
 import postRouter from './routes/post.route.js'
 
 const app = express();
+dotenv.config()
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,10 +16,13 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRouter);
-const CONNECTION_URL = 'mongodb+srv://huy:huy@cluster0.f40zo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+// const CONNECTION_URL = 'mongodb+srv://huy:huy@cluster0.f40zo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
